8a924b097553852539b3b67b1fc60a18bf8c3af9
ItaloCabral <g1brunoyokima@hotmail.com>
2022-07-10 23:18:39 -0300

    Feat(Pages): Movie and Search pages, still empty

---
+++ src/pages/Movie.tsx
@@ -0,0 +1,10 @@
+import { Path } from "react-router-dom";
+
+export function Movie() {
+
+
+
+  return (
+    <h1>Movie</h1>
+  );
+}
---
+++ src/pages/Search.tsx
@@ -0,0 +1,5 @@
+export function Search() {
+  return (
+    <h1>Search</h1>
+  );
+}
