6e0a069704bbb208ac5f5b2e78f7b1992965dbb9
ItaloCabral <g1brunoyokima@hotmail.com>
2022-07-10 23:17:58 -0300

    Feat(Router): Router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { Home } from 'pages';
+import { Home, Movie, Search } from 'pages';
 
 export const Router = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/movie/:id" element={<Movie />} />
+    <Route path="/search" element={<Search />} />
     <Route path="*" element={<Navigate to="/" />} />
   </Routes>
 );
